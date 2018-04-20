0069e919af3a07ee975507469210a7241949858a
kaeyleo <mynamelky@gmail.com>
2018-04-20 22:49:17 +0800

    refactor: directory structure

---
+++ site/views/home/main.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <img src="@/assets/logo.png">
+    <p>{{ desc }}</p>
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    margin-top: 32vh;
+  }
+  img {
+    width: 20vw;
+  }
+  p {
+    margin-top: 3vh;
+    font-size: 1.6vw;
+    color: #4E5667;
+    letter-spacing: .1vw;
+  }
+</style>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      desc: 'A Desktop UI Library For Vue.js'
+    }
+  }
+}
+</script>
